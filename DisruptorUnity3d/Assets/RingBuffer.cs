4c6d53453a02a87037599add05e729a16e5d17d5
Dave Hillier <dave@hillier.co>
2015-03-09 22:48:22 +0000

    Formatting

--- DisruptorUnity3d/Assets/RingBuffer.cs
+++ DisruptorUnity3d/Assets/RingBuffer.cs
@@ -4,83 +4,83 @@ using System.Threading;
 
 namespace DisruptorUnity3d
 {
-	public class RingBuffer<T> 
-	{
-	    private readonly T[] _entries;
-	    private readonly int _modMask;
-	    private Volatile.PaddedLong _consumerCursor = new Volatile.PaddedLong();
-	    private Volatile.PaddedLong _producerCursor = new Volatile.PaddedLong();
+    public class RingBuffer<T>
+    {
+        private readonly T[] _entries;
+        private readonly int _modMask;
+        private Volatile.PaddedLong _consumerCursor = new Volatile.PaddedLong();
+        private Volatile.PaddedLong _producerCursor = new Volatile.PaddedLong();
 
-	    public RingBuffer(int size)
-	    {
-	        size = NextPowerOfTwo(size);
-	        _modMask = size - 1;
-	        _entries = new T[size];
-	    }
+        public RingBuffer(int size)
+        {
+            size = NextPowerOfTwo(size);
+            _modMask = size - 1;
+            _entries = new T[size];
+        }
 
-	    public int Capacity
-	    {
-	        get { return _entries.Length; }
-	    }
+        public int Capacity
+        {
+            get { return _entries.Length; }
+        }
 
-	    public T this[long index]
-	    {
-	        get { unchecked { return _entries[index & _modMask]; } }
-	        set { unchecked { _entries[index & _modMask] = value; } }
-	    }
+        public T this[long index]
+        {
+            get { unchecked { return _entries[index & _modMask]; } }
+            set { unchecked { _entries[index & _modMask] = value; } }
+        }
 
-	    public T Dequeue()
-	    {
-	        var next = _consumerCursor.ReadFullFence() + 1;
-	        while (_producerCursor.ReadFullFence() < next)
-	        {
-	            Thread.SpinWait(1); 
-	        }
-	        _consumerCursor.WriteFullFence(next);
-	        return this[next];
-	    }
+        public T Dequeue()
+        {
+            var next = _consumerCursor.ReadFullFence() + 1;
+            while (_producerCursor.ReadFullFence() < next)
+            {
+                Thread.SpinWait(1);
+            }
+            _consumerCursor.WriteFullFence(next);
+            return this[next];
+        }
 
-	    public bool TryDequeue(out T obj)
-	    {
-	        var next = _consumerCursor.ReadFullFence() + 1;
+        public bool TryDequeue(out T obj)
+        {
+            var next = _consumerCursor.ReadFullFence() + 1;
 
-	        if (_producerCursor.ReadFullFence() < next)
-	        {
-	            obj = default(T);
-	            return false;
-	        }
-	        obj = Dequeue();
-	        return true;
-	    }
+            if (_producerCursor.ReadFullFence() < next)
+            {
+                obj = default(T);
+                return false;
+            }
+            obj = Dequeue();
+            return true;
+        }
 
-	    public void Enqueue(T element)
-	    {
-	        var next = _producerCursor.ReadFullFence() + 1;
+        public void Enqueue(T element)
+        {
+            var next = _producerCursor.ReadFullFence() + 1;
 
-	        long wrapPoint = next- _entries.Length;
-	        long min = _consumerCursor.ReadFullFence();
-	        
-	        while (wrapPoint > min)
-	        {
-	            min = _consumerCursor.ReadFullFence();
-	            Thread.SpinWait(1);
-	        }
+            long wrapPoint = next - _entries.Length;
+            long min = _consumerCursor.ReadFullFence();
 
-	        _producerCursor.WriteUnfenced(next);
-	        this[next] = element;
-	    }
+            while (wrapPoint > min)
+            {
+                min = _consumerCursor.ReadFullFence();
+                Thread.SpinWait(1);
+            }
 
-	    public int Count { get { return (int)(_producerCursor.ReadFullFence() - _consumerCursor.ReadFullFence()); } }
+            _producerCursor.WriteUnfenced(next);
+            this[next] = element;
+        }
 
-	    private static int NextPowerOfTwo(int x)
-	    {
-	        var result = 2;
-	        while (result < x)
-	        {
-	            result <<= 1;
-	        }
-	        return result;
-	    }
+        public int Count { get { return (int)(_producerCursor.ReadFullFence() - _consumerCursor.ReadFullFence()); } }
+
+        private static int NextPowerOfTwo(int x)
+        {
+            var result = 2;
+            while (result < x)
+            {
+                result <<= 1;
+            }
+            return result;
+        }
 
         public static class Volatile
         {
@@ -241,7 +241,7 @@ namespace DisruptorUnity3d
             }
         }
 
-	}
+    }
 
- 
+
 }
